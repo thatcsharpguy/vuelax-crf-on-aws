e29bff80998b9a995b34ccc952070f3c183e752d
Antonio Feregrino <antonio.feregrino@gmail.com>
2020-07-05 12:18:26 +0100

    improved training

--- training/main.py
+++ training/main.py
@@ -4,6 +4,7 @@ import joblib
 from sklearn.model_selection import train_test_split
 
 import csv
+from pipeline import full_pipeline
 
 offer_id_tokens = defaultdict(list)
 with open("./training/data/to_label-done.csv") as fd:
@@ -22,15 +23,14 @@ with open("./training/data/vuelos.csv") as fd:
 offers = []
 labels = []
 for offer_id, offer in enumerate(all_offers):
-    if offer_id not in offer_id_tokens or offer_id > 20:
+    if offer_id not in offer_id_tokens:
         break
     offers.append(offer)
     labels.append(offer_id_tokens[offer_id])
 
 x_train, x_test, y_train, y_test = train_test_split(offers, labels)
 
-from pipeline import full_pipeline
 
 full_pipeline.fit(x_train, y_train)
 
-joblib.dump(full_pipeline, "crf_pipeline.pkl")
+joblib.dump(full_pipeline, "vuelax.pkl")
